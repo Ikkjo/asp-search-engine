784aa68510d86f3cc543f830d279e81b58bcfda9
Ilija Kalinić <ilija.kalinic@gmail.com>
2020-07-04 19:55:12 +0200

    Added file graph structure

--- word_search/data_structure_initialisation.py
+++ word_search/data_structure_initialisation.py
@@ -2,16 +2,17 @@ import os
 import pickle
 
 from word_search.html_parser import Parser
-from word_search.trie import Trie, WordLocation
+from word_search.trie import Trie, WordLocations
 
 separator = os.sep
 DEFAULT_ROOT = os.curdir
 
-def initialise_trie(root_dir=DEFAULT_ROOT, console_log=False):
+
+def initialise_structures(root_dir=DEFAULT_ROOT, console_log=False):
     """Initialises trie from all .html files (recursively) starting from root_dir
 
     Tries to find an already made trie file in root_dir\data\trie.data. If file trie.data is not found, the funcion
-    searches for all .hmtl files starting from the root dir, parses the text from them and adds them to a Trie structure
+    searches for all .html files starting from the root dir, parses the text from them and adds them to a Trie structure
     and then saves that object in a binary file using the pickle library (file is saved in root_dir\data\ folder)
 
     :param root_dir: root_directory from which to search fro .html files
@@ -20,27 +21,31 @@ def initialise_trie(root_dir=DEFAULT_ROOT, console_log=False):
     if console_log:
         print("Loading trie data from file...")
 
-    trie_data_path = root_dir + separator+ "word_search"+ separator + "data" + separator + "trie.data"
+    trie_data_path = root_dir + separator + "word_search" + separator + "data" + separator + "trie.data"
+    file_graph_path = root_dir + separator + "word_search" + separator + "data" + separator + "file_graph.data"
 
-    if os.path.exists(trie_data_path) and os.path.isfile(trie_data_path):
-        trie = load_trie(trie_data_path)
+    if (os.path.exists(trie_data_path) and os.path.isfile(trie_data_path)) and\
+       (os.path.exists(file_graph_path) and os.path.isfile(file_graph_path)):  # Check to see if trie and graph data
+                                                                               # files exist
+        trie, graph = load_structures(trie_data_path, file_graph_path)
 
     else:
         if console_log:
             print("No data file found. Making trie...")
-        trie = make_trie()
+        trie, graph = make_structures()
         if console_log:
             print("Saving trie to file...")
-        save_trie(trie_data_path, trie)
+        save_structures(trie_data_path, file_graph_path, trie, graph)
 
     if console_log:
         print("Trie loaded.")
-    return trie
+    return trie, graph
 
 
-def make_trie():
+def make_structures():
     separator = os.sep
     trie = Trie()
+    file_graph = Graph()
     for (dirpath, dirnames, filenames) in os.walk("."):
         for file in filenames:
 
@@ -49,24 +54,37 @@ def make_trie():
                 path = dirpath + separator + file
                 html_file_contents = Parser().parse(path)
                 word_list = html_file_contents[1]
+                links = html_file_contents[0]
 
                 for word, index in zip(word_list, range(len(word_list))):
                     file_path = path
-                    location = WordLocation(file_path, index)
+                    location = WordLocations(file_path, index, links)
                     trie.add_word(word.lower(), location)
     return trie
 
 
-def save_trie(file_path, trie):
+def load_structures(trie_path, graph_path):
+    trie = load_from_file(trie_path)
+    graph = load_from_file(graph_path)
+    return trie, graph
+
+
+def save_structures(trie_path, graph_path, trie, graph):
+    save_to_file(trie_path, trie)
+    save_to_file(graph_path, graph)
+
+
+def save_to_file(file_path, trie):
     with open(file=file_path, mode="wb") as trie_file:
         pickle.dump(trie, file=trie_file)
 
-def load_trie(file_path):
+
+def load_from_file(file_path):
     with open(file=file_path, mode="rb") as trie_file:
         trie = pickle.load(trie_file)
     return trie
 
 
-if __name__ == '__main__':
-    trie = initialise_trie(".")
-    a = ""
+# if __name__ == '__main__':
+#     trie = initialise_structures(".")
+#     a = ""
