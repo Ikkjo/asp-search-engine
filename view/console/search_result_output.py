7604f0b7ca927d8e01aa55bee554485db6187a12
Ilija Kalinić <ilija.kalinic@gmail.com>
2020-07-05 23:34:05 +0200

    Fixed output parenthesis

--- view/console/search_result_output.py
+++ view/console/search_result_output.py
@@ -8,4 +8,4 @@ def print_results(results: list, abs=True):
         word = out["word"]
         path = out["path"]
 
-        print(f"Searched for {word}, found in file {path} (Rank: {rank}")
+        print(f"Searched for {word}, found in file {path} (Rank: {rank})")
